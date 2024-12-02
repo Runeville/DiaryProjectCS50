a258bcbed773df1c7d0a5c0487795910d6cbaf09
Vladimir <varkhipov2004@gmail.com>
2024-12-02 14:05:58 +0700

    Setting up flask

---
+++ app.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask, redirect, render_template, request, session
+from flask_session import Session
+from werkzeug.security import check_password_hash, generate_password_hash
+
+app = Flask(__name__)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+@app.route("/")
+def hello_world():
+    return "<h1>Hello World!</h1>"
