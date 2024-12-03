5a92ce7a58082a441fa9362344736ca27e49acc0
Vladimir <varkhipov2004@gmail.com>
2024-12-03 13:01:39 +0700

    Created login_required() in helpers.py, set up those new register and login features in app.py, modified http requests

--- app.py
+++ app.py
@@ -4,6 +4,8 @@ from flask import Flask, redirect, render_template, request, session
 from flask_session import Session
 from werkzeug.security import check_password_hash, generate_password_hash
 
+from helpers import login_required
+
 from database import init_db, db_session
 from models import User
 
@@ -16,6 +18,16 @@ app.config["SESSION_TYPE"] = "filesystem"
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///diary.db"
 
 init_db()
+
+@app.after_request
+def after_request(response):
+    """Ensure responses aren't cached"""
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+
 Session(app)
 
 
@@ -25,5 +37,23 @@ def shutdown_session(exception=None):
 
 
 @app.route("/")
+@login_required
 def hello_world():
     return render_template("index.html")
+
+
+@app.route("/login", methods = ["GET", "POST"])
+def login():
+    return render_template("login.html")
+
+
+@app.route("/register", methods = ["GET", "POST"])
+def register():
+    return render_template("register.html")
+
+
+@app.route("/logout")
+def logout():
+    session.clear()
+
+    return redirect("/")
